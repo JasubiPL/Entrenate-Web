6a3aa025354c0486a621bf2924dbd55ea85db025
JasubiP <gameportal7@hotmail.com>
2023-05-07 13:07:59 -0600

    added banner in home

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { SiFacebook } from 'react-icons/si'
+import { FcSportsMode } from 'react-icons/fc'
 
 export default function Nav() {
   return (
     <nav className='w-full bg-gray-900 py-2 px-4 flex justify-between items-center'>
-      <Link href='/' className=''>
+      <Link href='/' className='flex items-center gap-2'>
         <h1 className='text-white text-2xl lg:text-4xl'>Piñeyro-Fit</h1>
+        <FcSportsMode className="text-3xl"/>
       </Link>
       <section className=''>
         <a href='https://www.facebook.com/entrenate' target="_blanck">
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,15 +8,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <PrincipalLayout title='Piñeyro Fit'>
-      <main className=' min-h-screen flex justify-center bg-gray-950 py-4 text-white'>
-        <section className='w-full max-w-5xl flex flex-wrap gap-6 p-4 justify-center items-start lg:items-center
+      <main className=' min-h-screen flex flex-col items-center px-2 gap-4 bg-gray-950 py-4 text-white'>
+        <section className="w-full flex max-w-5xl h-36 lg:h-64 filter grayscale relative">
+          <img src="/image/banner.jpg" alt="banner principal" 
+            className="object-cover rounded-xl"
+          />
+          <h2 className="absolute w-full h-full grid place-content-center text-5xl md:text-7xl">Cambia tu vida</h2>
+        </section>
+        <section className='w-full max-w-5xl flex flex-wrap gap-6  justify-center items-start lg:items-center
         lg:flex-nowrap'>
           <Link href='/ejersicioencasa' className='group bg-gray-900 rounded-xl w-full max-w-sm p-6 flex flex-col items-center
           gap-4 text-center border-2 border-gray-900 hover:border-red-500 
           hover:shadow-lg hover:shadow-red-200 transition-all active:scale-95'>
             <TbHomeBolt className='text-6xl group-hover:text-red-500'/>
             <h2 className='text-2xl'>Ejercicio en Casa</h2>
-            <p>
+            <p className="text-sm font-light">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Temporibus delectus sed omnis quia qui, rem, libero autem nam vero ducimus 
               maiores illo aliquam veritatis hic cumque, perspiciatis in quaerat! Ipsam
@@ -27,7 +33,7 @@ export default function Home() {
           hover:shadow-lg hover:shadow-green-200 transition-all'>
             <GiGymBag className='text-6xl group-hover:text-green-500'/>
             <h2 className='text-2xl'>Ejercicio en GYM</h2>
-            <p>
+            <p className="text-sm font-light">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Temporibus delectus sed omnis quia qui, rem, libero autem nam vero ducimus 
               maiores illo aliquam veritatis hic cumque, perspiciatis in quaerat! Ipsam
@@ -38,7 +44,7 @@ export default function Home() {
           hover:shadow-lg hover:shadow-blue-200 transition-all'>
             <TbTrees className='text-6xl group-hover:text-blue-500' />
             <h2 className='text-2xl'>Ejercicio en Area Libre</h2>
-            <p>
+            <p className="text-sm font-light">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Temporibus delectus sed omnis quia qui, rem, libero autem nam vero ducimus 
               maiores illo aliquam veritatis hic cumque, perspiciatis in quaerat! Ipsam
