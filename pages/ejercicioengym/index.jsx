2303d756188fa16dccdfeb4965d98590d4c2f229
JasubiP <gameportal7@hotmail.com>
2023-05-07 16:29:05 -0600

    Icons are added to the exercise menu in Gym

--- pages/ejercicioengym/index.jsx
+++ pages/ejercicioengym/index.jsx
@@ -10,51 +10,63 @@ export default function GymExcercisesPage() {
         <section className="w-full max-w-5xl gap-6 p-4">
           <ul className="w-full flex flex-wrap justify-evenly gap-5">
             <li>
-              <Link href='/ejercicioengym/abdomen' className="dev-list-mobile-animation-up group rounded-full bg-slate-900 aspect-square flex 
-              flex-col items-center justify-center gap-2 w-36 border-2 border-slate-900 lg:hover:border-green-500 
-              lg:hover:scale-110 transition-all lg:active:scale-95">
-                <GiAbdominalArmor className="text-5xl group-hover:text-green-500"/>
-                <h2 className="text-xl">Abdominales</h2>
+              <Link href='/ejercicioengym/abdomen' className=" lg:w-56 dev-list-mobile-animation-up group rounded-xl bg-slate-900 aspect-square flex 
+              flex-col items-center justify-center gap-2 lg:gap-4 w-36 border-2 border-slate-900 lg:hover:border-green-500 
+              lg:hover:scale-110 transition-all lg:active:scale-95 group">
+                <img src="/image/icons/abs-icon.png" alt="icono pectoral" 
+                  className="w-1/2"
+                />
+                <h2 className="text-xl lg:text-3xl lg:group-hover:text-green-500">Abdominales</h2>
               </Link>
             </li>
             <li>
-              <Link href='/ejercicioengym/pecho' className="dev-list-mobile-animation-up group rounded-full bg-slate-900 aspect-square flex 
-              flex-col items-center justify-center gap-2 w-36 border-2 border-slate-900 lg:hover:border-green-500 
-              lg:hover:scale-110 transition-all lg:active:scale-95">
-                <GiAbdominalArmor className="text-5xl group-hover:text-green-500"/>
-                <h2 className="text-xl">Pecho</h2>
+              <Link href='/ejercicioengym/pecho' className="dev-list-mobile-animation-up group rounded-xl bg-slate-900 aspect-square flex 
+              flex-col items-center justify-center gap-1 w-36 border-2 border-slate-900 lg:hover:border-green-500 
+              lg:hover:scale-110 transition-all lg:active:scale-95 lg:w-56 lg:gap-4 group" >
+                <img src="/image/icons/chest-icon.png" alt="icono pectoral" 
+                  className="w-1/2"
+                />
+                <h2 className="text-xl lg:text-3xl lg:group-hover:text-green-500">Pecho</h2>
               </Link>
             </li>
             <li>
-              <Link href='/ejercicioengym/espalda' className="dev-list-mobile-animation-up group rounded-full bg-slate-900 aspect-square flex 
+              <Link href='/ejercicioengym/espalda' className="dev-list-mobile-animation-up group rounded-xl bg-slate-900 aspect-square flex 
               flex-col items-center justify-center gap-2 w-36 border-2 border-slate-900 lg:hover:border-green-500 
-              lg:hover:scale-110 transition-all lg:active:scale-95">
-                <GiAbdominalArmor className="text-5xl group-hover:text-green-500"/>
-                <h2 className="text-xl">Espalda</h2>
+              lg:hover:scale-110 transition-all lg:active:scale-95 lg:w-56 lg:gap-4 group">
+                <img src="/image/icons/back-icon.png" alt="icono pectoral" 
+                  className="w-1/2"
+                />
+                <h2 className="text-xl lg:text-3xl lg:group-hover:text-green-500">Espalda</h2>
               </Link>
             </li>
             <li>
-              <Link href='/ejercicioengym/hombros' className="dev-list-mobile-animation-up group rounded-full bg-slate-900 aspect-square flex 
+              <Link href='/ejercicioengym/hombros' className="dev-list-mobile-animation-up group rounded-xl bg-slate-900 aspect-square flex 
               flex-col items-center justify-center gap-2 w-36 border-2 border-slate-900 lg:hover:border-green-500 
-              lg:hover:scale-110 transition-all lg:active:scale-95">
-                <GiAbdominalArmor className="text-5xl group-hover:text-green-500"/>
-                <h2 className="text-xl">Hombros</h2>
+              lg:hover:scale-110 transition-all lg:active:scale-95 lg:w-56 lg:gap-4 group">
+                <img src="/image/icons/shoulders-icon.png" alt="icono pectoral" 
+                  className="w-1/2"
+                />
+                <h2 className="text-xl lg:text-3xl lg:group-hover:text-green-500">Hombros</h2>
               </Link>
             </li>
             <li>
-              <Link href='/ejercicioengym/brazo' className="dev-list-mobile-animation-up group rounded-full bg-slate-900 aspect-square flex 
+              <Link href='/ejercicioengym/brazo' className="dev-list-mobile-animation-up group rounded-xl bg-slate-900 aspect-square flex 
               flex-col items-center justify-center gap-2 w-36 border-2 border-slate-900 lg:hover:border-green-500 
-              lg:hover:scale-110 transition-all lg:active:scale-95">
-                <GiAbdominalArmor className="text-5xl group-hover:text-green-500"/>
-                <h2 className="text-xl">Brazo</h2>
+              lg:hover:scale-110 transition-all lg:active:scale-95 lg:w-56 lg:gap-4 group">
+                <img src="/image/icons/arm-icon.png" alt="icono pectoral" 
+                  className="w-1/2"
+                />
+                <h2 className="text-xl lg:text-3xl lg:group-hover:text-green-500">Brazo</h2>
               </Link>
             </li>
             <li>
-              <Link href='/ejercicioengym/pierna' className="dev-list-mobile-animation-up group rounded-full bg-slate-900 aspect-square flex 
+              <Link href='/ejercicioengym/pierna' className="dev-list-mobile-animation-up group rounded-xl bg-slate-900 aspect-square flex 
               flex-col items-center justify-center gap-2 w-36 border-2 border-slate-900 lg:hover:border-green-500 
-              lg:hover:scale-110 transition-all lg:active:scale-95 min-[]:">
-                <GiAbdominalArmor className="text-5xl group-hover:text-green-500"/>
-                <h2 className="text-xl">Pierna</h2>
+              lg:hover:scale-110 transition-all lg:active:scale-95 lg:w-56 lg:gap-4 group">
+                <img src="/image/icons/legs-icon.png" alt="icono pectoral" 
+                  className="w-1/2"
+                />
+                <h2 className="text-xl lg:text-3xl lg:group-hover:text-green-500">Pierna</h2>
               </Link>
             </li>
           </ul>
