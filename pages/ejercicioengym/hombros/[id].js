f4e3e0379caa9d7954cdce24a7e694fc7919c157
JasubiPL <gameportal7@hotmail.com>
2023-05-29 16:09:15 -0600

    Added Shoulder excercise whitout machine

---
+++ pages/ejercicioengym/hombros/[id].js
@@ -0,0 +1,69 @@
+import { readFile, readdir } from "fs/promises"
+import { basename } from "path"
+import PrincipalLayout from "@/components/PrincipalLayout"
+
+export default function BackExcercise({ img, name, muscles_involved, execution, caution, }){
+  return(
+    <PrincipalLayout title={name}>
+      <main className="w-full items-center flex flex-col mb-8 text-white dev-animation-up">
+        <img src={img} alt={name}  className="w-full lg:max-w-4xl lg:mt-8"/>
+        <section className="w-full lg:max-w-4xl flex flex-col gap-4 p-2">
+          <h1 className="text-3xl lg:text-5xl lg:py-8">{name}</h1>
+          <ol>
+            <label className="text-md lg:text-2xl font-bold text-red-500">🔥 Musculos afectados</label>
+            <li className="text-sm lg:text-lg font-light">
+              <span className="font-semibold">Principal: </span> 
+              {muscles_involved.principal}
+            </li>
+            <li className="text-sm lg:text-lg font-light">
+              <span className="font-semibold">Secundario: </span> 
+              {muscles_involved.secundary}
+            </li>
+            <li className="text-sm lg:text-lg font-light">
+              <span className="font-semibold">Antagonista: </span> 
+              {muscles_involved.antagonist}
+            </li>
+          </ol>
+          <section>
+            <label className="text-md lg:text-2xl font-bold text-green-500">✅ Ejecución </label><br />
+            <p className="text-sm lg:text-lg font-light">{execution}</p>
+          </section>
+          <section>
+            <label className="text-md lg:text-2xl font-bold text-yellow-300">⚠️ Errores frecuentes </label><br />
+            <p className="text-sm lg:text-lg font-light">{caution}</p>
+          </section>
+        </section>
+      </main>
+    </PrincipalLayout>
+  )
+}
+
+export async function getStaticPaths(){
+  const files = await readdir("./excercises/gym-excercises/shoulder", "utf-8")
+  
+
+  const paths = files.map(file => {
+    const id = basename(file, ".json")
+    return{ params: { id } }
+  })
+
+  return{
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }){
+  const { id } = params
+
+  const content = await readFile(`./excercises/gym-excercises/shoulder/${id}.json`, "utf-8")
+  const excercise = JSON.parse(content)
+
+  return {
+    props: {
+     ...excercise,
+    }
+  }
+
+
+}
---
+++ pages/ejercicioengym/hombros/index.jsx
@@ -0,0 +1,74 @@
+import PrincipalLayout from "@/components/PrincipalLayout";
+import fs from "fs/promises"
+import Link from "next/link";
+import path from "path";
+import { GiGymBag } from 'react-icons/gi'
+
+export default function BackExcercisesMenu({ excercises }) {
+
+  return (
+    <PrincipalLayout title='Ejersicios para hombro'>
+      <main className="w-full flex min-h-screen justify-center bg-slate-950 text-white">
+        <section className="w-full max-w-5xl p-2">
+          <h2 className="text-3xl lg:text-5xl flex gap-3 my-4">
+            Grupo Hombros <GiGymBag className="text-green-500"/> 
+          </h2>
+          <section className=" rounded-xl mt-2 mb-5 overflow-hidden dev-list-mobile-animation">
+            <img src="/image/gym-excercises/shoulder/grupo-hombros.png" alt="ejersicios Grupo dorsales" />
+          </section>
+          <h2 className="text-2xl lg:text-4xl">Ejercicios</h2>
+          <ul className="w-full max-w-5xl flex flex-col gap-3 md:flex-row md:flex-wrap md:justify-evenly">
+            {excercises.map(excercise =>{
+              return (
+                <Link key={excercise.id} href={`/ejercicioengym/hombros/${excercise.id}`}>
+                  <li className="w-full h-20 flex gap-2 bg-slate-900 rounded-md overflow-hidden dev-list-mobile-animation
+                  md:w-60 md:flex-col md:h-full lg:border-4 lg:hover:-translate-y-2 lg:rounded-xl lg:border-slate-950
+                   lg:hover:border-green-500 transition-all">
+                    <div className="h-full aspect-square lg:aspect-video flex md:h-auto md:w-full">
+                      <img src={excercise.img} alt={excercise.title} 
+                      className="w-full object-cover"
+                      loading="lazy"
+                      />
+                    </div>
+                    <div className="p-2">
+                      <h3 className="md:text-xl">{excercise.name}</h3>
+                      <p className="text-sm font-light">{excercise.muscles_involved.principal}</p>
+                    </div>
+                  </li>
+                </Link>
+              )
+            })}
+          </ul>
+        </section>
+      </main>
+    </PrincipalLayout>
+  );
+};
+
+export async function getStaticProps() {
+  const dirPath = path.join(process.cwd(), './excercises/gym-excercises/shoulder');
+  const fileNames = await fs.readdir(dirPath);
+
+  const excercises = await Promise.all(fileNames.map(async (fileName) => {
+    const filePath = path.join(dirPath, fileName);
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    const excercise = JSON.parse(fileContent);
+    return excercise;
+  }));
+
+  //console.log(excercises)
+
+  return {
+    props: {
+      excercises,
+    },
+  };
+}
+
+
+
+
+
+
+
+
