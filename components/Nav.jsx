b59cbd4401b2d5b8dbfb671a85af19e84896ab05
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-01-28 09:38:33 -0600

    Se cambiaron iconos y añadio el logotipo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import { IoIosFitness } from "react-icons/io";
 import { IoNutritionOutline } from "react-icons/io5";
 import { LuListTodo } from "react-icons/lu";
 import { BsClipboard2Data } from "react-icons/bs";
-import { CgFeed } from "react-icons/cg";
+import { GrArticle } from "react-icons/gr";
 import { useEffect, useState } from "react";
 
 export default function Nav() {
@@ -22,7 +22,10 @@ export default function Nav() {
 
   return (
     <nav className='w-full bg-gray-900 flex flex-wrap p-3 justify-between items-center'>
-      <h1 className='text-white text-2xl lg:text-4xl w-full lg:w-auto'>Entrenate</h1>   
+      <span className="w-full lg:w-auto flex gap-2 items-center">
+        <img className="w-10 rounded-full" src="/image/entrenate_logo.png" alt="Logotipo"/>
+        <h1 className='text-white text-2xl lg:text-4xl'>Entrenate</h1> 
+      </span>   
       <ul className="w-full flex justify-evenly lg:w-auto gap-3 text-white lg:flex lg:mr-4">
         <li>
           <Link className={`flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-green-400 
@@ -32,26 +35,26 @@ export default function Nav() {
           </Link>
         </li>
         <li>
-          <Link className={`flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-orange-400 `} href='/nutricion'>
+          <Link className={`flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-green-400 `} href='/nutricion'>
             <IoNutritionOutline />
             <span className={`${ widthScreen}`}>Ntutricion</span>
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-orange-400" href='/rutinas'>
-            <BsClipboard2Data />
+          <Link className="flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-green-400" href='/rutinas'>
+            <LuListTodo />
             <span className={`${ widthScreen}`}>Rutinas</span>
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-orange-400" href='/Blog'>
-            <CgFeed />
+          <Link className="flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-green-400" href='/Blog'>
+            <GrArticle />
             <span className={`${ widthScreen}`}>BLog</span>
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-orange-400" href='/mi-plan'>
-            <LuListTodo />
+          <Link className="flex gap-2 text-2xl lg:text-lg items-center justify-center hover:text-green-400" href='/mi-plan'>
+            <BsClipboard2Data />
             <span className={`${ widthScreen}`}>Mi Plan</span>
           </Link>
         </li>
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,15 +10,10 @@ export default function Home() {
         <section className='w-full max-w-5xl flex flex-wrap gap-6  justify-center items-start lg:items-center
         lg:flex-nowrap'>
           <Link href='/ejercicioengym' className=' bg-gray-900 rounded-xl w-full max-w-sm p-6 flex flex-col items-center
-          gap-4 text-center hover:scale-105 hover:shadow-red-200 transition-all active:scale-95
+          gap-4 text-center transition-all active:scale-95
           dev-list-mobile-animation-up'>
             <GiGymBag className='text-6xl'/>
             <h2 className='text-2xl'>Ejercicio en GYM</h2>
-            <p className="text-sm font-light">
-              En el gimnasio, puedes disfrutar de una amplia gama de actividades físicas y 
-              entrenamientos personalizados según tus objetivos. Desde levantamiento de 
-              pesas hasta entrenamiento funcional, hay algo para todos.
-            </p>
           </Link>
         </section>
       </main>
